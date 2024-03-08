36472f680eccfa623337b5adf328cc3b8659cdea
Jimmy Givans <jauyoma.dev@gmail.com>
2024-03-08 21:50:10 +0300

    feat: add messages array

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,6 +1,19 @@
 import express from "express";
 import ViteExpress from "vite-express";
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 const app = express();
 
 ViteExpress.listen(app, 3000, () => console.log("Server is listening on port 3000..."));
