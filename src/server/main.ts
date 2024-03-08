8114ba3dd117d776a73899eb4f49efdbbf833c63
Jimmy Givans <jauyoma.dev@gmail.com>
2024-03-08 22:00:12 +0300

    feat: add route for getting a message by its ID

--- src/server/main.ts
+++ src/server/main.ts
@@ -20,4 +20,14 @@ app.get("/api/messages", (req, res) => {
   res.json(messages);
 });
 
+app.get("/api/messages/:messageID", (req, res) => {
+  const id = Number(req.params.messageID);
+
+  if (id >= messages.length) {
+    res.status(400).json({ message: "Message does not exist" });
+  }
+
+  res.json(messages[id - 1]);
+});
+
 ViteExpress.listen(app, 3000, () => console.log("Server is listening on port 3000..."));
