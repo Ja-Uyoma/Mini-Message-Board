3eb901a7b52609a597ceb0f9a4fe00de7a980d2a
Jimmy Givans <jauyoma.dev@gmail.com>
2024-03-08 22:28:35 +0300

    feat: add route for creating new posts

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import ViteExpress from "vite-express";
+import { body, validationResult } from "express-validator";
 
 const messages = [
   {
@@ -16,6 +17,8 @@ const messages = [
 
 const app: Express = express();
 
+app.use(express.json());
+
 app.get("/api/messages", (req: Request, res: Response) => {
   res.json(messages);
 });
@@ -29,5 +32,25 @@ app.get("/api/messages/:messageID", (req: Request, res: Response) => {
 
   res.json(messages[id - 1]);
 });
+
+app.post(
+  "/api/messages",
+  [
+    body("text").isString().notEmpty().withMessage("Text cannot be empty!"),
+    body("user").isString().notEmpty().withMessage("User cannot be empty!"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    }
+
+    const { text, user }: { text: string; user: string } = req.body;
+
+    messages.push({ text: text, user: user, added: new Date() });
+    res.json(messages[messages.length - 1]);
+  }
+);
 
 ViteExpress.listen(app, 3000, () => console.log("Server is listening on port 3000..."));
