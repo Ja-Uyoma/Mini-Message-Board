4de5c62f1d204b6b4705749745d69278972a973c
Jimmy Givans <jauyoma.dev@gmail.com>
2024-03-08 23:36:38 +0300

    feat: render a custom message on fetching messages from server

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,11 +1,31 @@
 import "./App.css";
 
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const getter = async () => {
+      try {
+        const res = await fetch("/api/messages");
+        const data = await res.json();
+        console.log(data);
+        setMessages(data);
+      }
+      catch (err) {
+        console.error(err);
+      }
+    };
+
+    getter();
+  }, []);
+
   return (
     <div className="App">
-      <p>Hello World!</p>  
+      {
+        messages.length >= 1 ?  messages.map((msg, idx) => <p key={idx}>This is a message</p> ) : <p>No messages to display</p>
+      } 
     </div>
   );
 }
