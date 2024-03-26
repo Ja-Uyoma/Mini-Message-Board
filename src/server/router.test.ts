413035c0ecc6ad871a60d064c38e16a49524dad7
Jimmy Givans <jauyoma.dev@gmail.com>
2024-03-26 16:09:57 +0300

    test: check that the response equals the messages object

--- src/server/router.test.ts
+++ src/server/router.test.ts
@@ -1,18 +1,22 @@
 import express from "express";
-import { describe, it } from "vitest";
+import { describe, expect, it } from "vitest";
 import supertest from "supertest";
 import { router } from "./router.ts";
+import messages from "./messages.ts";
 
 const app = express();
 
 app.use("/api", router);
 
 describe("GET /api/messages", function () {
-  it("responds with JSON", function () {
+  it("responds with the messages in JSON", function () {
     supertest(app)
       .get("/messages")
       .set("Accept", "application/json")
       .expect("Content-Type", /json/)
-      .expect(200);
+      .expect(200)
+      .then((res) => {
+        expect(res.body).toEqual(messages);
+      });
   });
 });
