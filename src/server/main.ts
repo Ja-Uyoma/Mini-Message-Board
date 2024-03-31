e2fdf2d8cfcfd8faabff1d3d7899722d90d39491
Jimmy Givans <jauyoma.dev@gmail.com>
2024-03-31 19:22:38 +0300

    feat: use morgan to log requests to the console

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,11 +1,13 @@
 import express, { Express } from "express";
 import ViteExpress from "vite-express";
+import logger from "morgan";
 
 import { router } from "./router.js";
 
 const app: Express = express();
 
 app.use(express.json());
+app.use(logger("dev"));
 app.use("/api", router);
 
 ViteExpress.listen(app, 3000, () => console.log("Server is listening on port 3000..."));
