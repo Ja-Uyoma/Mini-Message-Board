84be78145fed7f75b7149b595de1e387fd9ce7ff
Jimmy Givans <jauyoma.dev@gmail.com>
2024-03-31 22:06:45 +0300

    refactor: use async-await syntax in test cases

--- src/server/router.test.ts
+++ src/server/router.test.ts
@@ -4,52 +4,48 @@ import app from "./main.ts";
 import messages from "./messages.ts";
 
 describe("Returns the messages in the message object", function () {
-  test("GET /api/message returns all messages", function () {
-    return supertest(app)
+  test("GET /api/message returns all messages", async function () {
+    const res = await supertest(app)
       .get("/api/messages")
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(200)
-      .then((res) => {
-        expect(res.body).toBeDefined();
-        expect(res.body).toEqual(messages);
-      })
+      .set("Accept", "application/json");
+
+    expect(res.type).toBe("application/json");
+    expect(res.status).toEqual(200);
+    expect(res.body).toBeDefined();
+    expect(res.body).toEqual(messages);
   });
 
-  test("GET /api/messages/1 returns the first message", function () {
-    return supertest(app)
+  test("GET /api/messages/1 returns the first message", async function () {
+    const res = await supertest(app)
       .get("/api/messages/1")
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(200)
-      .then((res) => {
-        expect(res.body).toBeDefined();
-        expect(res.body).toEqual(messages[0]);
-      });
+      .set("Accept", "application/json");
+
+    expect(res.type).toBe("application/json");
+    expect(res.status).toEqual(200);
+    expect(res.body).toBeDefined();
+    expect(res.body).toEqual(messages[0]);
   });
 
-  test("GET /api/messages/2 returns the second message", function () {
-    return supertest(app)
+  test("GET /api/messages/2 returns the second message", async function () {
+    const res = await supertest(app)
       .get("/api/messages/2")
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(200)
-      .then((res) => {
-        expect(res.body).toBeDefined();
-        expect(res.body).toEqual(messages[1]);
-      });
+      .set("Accept", "application/json");
+
+    expect(res.type).toBe("application/json");
+    expect(res.status).toEqual(200);
+    expect(res.body).toBeDefined();
+    expect(res.body).toEqual(messages[1]);
   });
 
-  test("GET /api/messages/100 handles retrieval of non-existent message", function () {
-    return supertest(app)
+  test("GET /api/messages/100 handles retrieval of non-existent message", async function () {
+    const res = await supertest(app)
       .get("/api/messages/100")
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(400)
-      .then((res) => {
-        expect(res.body).toBeDefined();
-        expect(res.body).toEqual({ message: "Message does not exist" });
-      });
+      .set("Accept", "application/json");
+
+    expect(res.type).toBe("application/json");
+    expect(res.status).toEqual(400);
+    expect(res.body).toBeDefined();
+    expect(res.body).toEqual({ message: "Message does not exist" });
   });
 });
 
@@ -65,15 +61,14 @@ describe("Create new messages", function () {
     expect(res.error).toBeDefined(); 
   });
 
-  test("POST /api/messages creates a new message", function () {
-    return supertest(app)
+  test("POST /api/messages creates a new message", async function () {
+    const res = await supertest(app)
       .post("/api/messages")
       .send({ text: "Amoso u ni okwe", user: "Mosh" })
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .then((res) => {
-        expect(res.status).toEqual(200);
-        expect(res.body).toBeDefined();
-      })
-  })
+      .set("Accept", "application/json");
+
+    expect(res.type).toBe("application/json");
+    expect(res.status).toEqual(200);
+    expect(res.body).toBeDefined();
+  });
 });
