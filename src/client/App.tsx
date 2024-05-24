3f5fde9f77fabe66262e96690743eb4d033e3641
Jimmy Givans <jauyoma.dev@gmail.com>
2024-05-24 13:14:15 +0300

    refactor: move Messages component to a new file
    - The file src/client/Messages.tsx now contains both the type of
      the message objects being rendered, as well as the components
      that handle the rendering

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,52 +1,7 @@
-import React, {useEffect, useState} from "react";
-import type { Message } from "./Message.type";
+import { Messages } from "./Messages"; 
 
 function App() {
   return <Messages />;
-}
-
-function Messages() {
-  const [messages, setMessages] = useState<Message[]>([]);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    fetch("/api/messages")
-      .then((res) => { if (res.status >= 400) { throw new Error(res.statusText); } else { return res.json(); } })
-      .then((messages) => { setMessages(messages); })
-      .catch((err) => { setError(err); })
-      .finally(() => { setIsLoading(false); } )
-  }, []);
-
-  if (error) {
-    return (
-      <div>
-        <p>A network error was encountered</p>
-      </div>
-    );
-  }
-
-  if (isLoading) {
-    return (
-      <div>
-        <p>Loading...</p>
-      </div>
-    );
-  }
-
-  return (
-    <div>
-      { messages.length >= 1 ? messages.map((msg, idx) => <Message key={idx} text={msg.text} user={msg.user} added={msg.added} />) : <p>No messages to display</p> }
-    </div>
-  );
-}
-
-function Message({ text, user, added }: Message) {
-  return (
-    <div>
-      {text} {user} {added}
-    </div>
-  );
 }
 
 export default App;
