36352bf30d8e8a6c230a6c92a98f1fa977607113
Jimmy Givans <jauyoma.dev@gmail.com>
2024-05-24 13:15:46 +0300

    chore: delete unused file src/client/Messages.type.ts

--- src/client/Message.type.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-const MessageSchema = z.object({
-    text: z.string(),
-    user: z.string(),
-    added: z.string().date(),
-});
-
-export type Message = z.infer<typeof MessageSchema>;
