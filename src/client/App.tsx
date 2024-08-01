804c3ebf1d97fb1debf66f64695040bca1c34c86
Jimmy Givans <jauyoma.dev@gmail.com>
2024-08-01 15:34:20 +0300

    feat: add a modal for creating new messages

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,7 +1,37 @@
 import { Messages } from "./Messages";
+import { useState } from "react";
 
 function Button() {
-  return <button type="button">Add a new message</button>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onButtonClicked = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <button type="button" onClick={onButtonClicked}>
+        Add a new message
+      </button>
+
+      <dialog open={isOpen}>
+        Add a new message
+        <form action="/api/messages" method="post">
+          <div>
+            <label htmlFor="text">Text</label>
+            <input type="text" name="text" id="text" />
+          </div>
+
+          <div>
+            <label htmlFor="user">User</label>
+            <input type="text" name="user" id="user" />
+          </div>
+
+          <button type="submit">Submit</button>
+        </form>
+      </dialog>
+    </>
+  );
 }
 
 function App() {
