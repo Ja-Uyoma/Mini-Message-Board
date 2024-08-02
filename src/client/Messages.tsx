72153b74914599bc62d6ab269f45bbded80977db
Jimmy Givans <jauyoma.dev@gmail.com>
2024-08-02 15:21:10 +0300

    feat: style the Message component

--- src/client/Messages.tsx
+++ src/client/Messages.tsx
@@ -11,8 +11,14 @@ type Message = z.infer<typeof MessageSchema>;
 
 function Message({ text, user, added }: Message) {
   return (
-    <div>
-      {text} {user} {added}
+    <div className="bg-gray-100 rounded my-5 p-3">
+      <p className="text-lg">{text}</p>
+      <div className="flex justify-between mt-3">
+        <p className="text-sm">
+          From: <span className="font-medium">{user}</span>
+        </p>
+        <p className="text-sm">{added}</p>
+      </div>
     </div>
   );
 }
