fffc5002d42856110ccc9e4aad9b00010d7e7d01
Jimmy Givans <jauyoma.dev@gmail.com>
2024-08-02 15:34:25 +0300

    refactor: move Message component into Message.tsx file

--- src/client/components/Messages.tsx
+++ src/client/components/Messages.tsx
@@ -1,27 +1,5 @@
-import { z } from "zod";
+import Message from "./Message";
 import { useState, useEffect } from "react";
-
-const MessageSchema = z.object({
-  text: z.string(),
-  user: z.string(),
-  added: z.string().date(),
-});
-
-type Message = z.infer<typeof MessageSchema>;
-
-function Message({ text, user, added }: Message) {
-  return (
-    <div className="bg-gray-100 rounded my-5 p-3">
-      <p className="text-lg">{text}</p>
-      <div className="flex justify-between mt-3">
-        <p className="text-sm">
-          From: <span className="font-medium">{user}</span>
-        </p>
-        <p className="text-sm">{added}</p>
-      </div>
-    </div>
-  );
-}
 
 function NetworkError() {
   return (
