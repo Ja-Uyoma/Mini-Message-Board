a2ef6cc8c6133a74788baf26bcb214164c71e220
Jimmy Givans <jauyoma.dev@gmail.com>
2024-08-02 22:00:49 +0300

    refactor: render the Dialog component in the App component

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,44 +1,5 @@
 import { Messages } from "./Messages";
-import { useState } from "react";
-
-function Form() {
-  return (
-    <form action="/api/messages" method="post">
-      <div>
-        <label htmlFor="text">Text</label>
-        <input type="text" name="text" id="text" />
-      </div>
-
-      <div>
-        <label htmlFor="user">User</label>
-        <input type="text" name="user" id="user" />
-      </div>
-
-      <button type="submit">Submit</button>
-    </form>
-  );
-}
-
-function Button() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const onButtonClicked = () => {
-    setIsOpen(!isOpen);
-  };
-
-  return (
-    <>
-      <button type="button" onClick={onButtonClicked}>
-        Add a new message
-      </button>
-
-      <dialog open={isOpen}>
-        Add a new message
-        <Form />
-      </dialog>
-    </>
-  );
-}
+import Dialog from "./Dialog";
 
 function App() {
   return (
@@ -48,7 +9,7 @@ function App() {
       </header>
       <main>
         <Messages />
-        <Button />
+        <Dialog />
       </main>
       <footer>
         <p>Copyright &copy; 2023 Mini Message Board. Created By Jimmy Givans</p>
