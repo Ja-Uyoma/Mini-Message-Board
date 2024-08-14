d7a08c95257a445b89c16d7eb1f276aa56aa7d09
Jimmy Givans <jauyoma.dev@gmail.com>
2024-08-14 15:34:48 +0300

    feat: make the ErrorPage component the Root's errorElement

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -5,11 +5,13 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Root from "./components/Root";
+import ErrorPage from "./components/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
