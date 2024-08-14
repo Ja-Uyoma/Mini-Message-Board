1d11991b76f215051b91e6fd549ef369f32608dd
Jimmy Givans <jauyoma.dev@gmail.com>
2024-08-14 15:40:46 +0300

    build: add routes subdirectory to tailwind content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/client/components/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/client/components/*.{js,ts,jsx,tsx}",
+    "./src/client/routes/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
