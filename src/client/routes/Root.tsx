ab81fbbfaee7e60044128f8123e165de47e26b83
Jimmy Givans <jauyoma.dev@gmail.com>
2024-08-20 16:26:15 +0300

    refactor: create a new Dialog component
    - We extract the logic for opening and closing the dialog modal
      into a new Dialog component which we then render in the Root
      component

--- src/client/routes/Root.tsx
+++ src/client/routes/Root.tsx
@@ -15,10 +15,55 @@ export async function loader() {
 
 function Root() {
   const messages = useLoaderData() as MessageObject[];
-  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <header className="my-3">
+        <h1 className="text-2xl text-center">Mini Message Board</h1>
+      </header>
+      <main className="w-2/4 my-0 mx-auto">
+        <h2 className="text-xl">Messages</h2>
+
+        <>
+          {messages.length >= 1 ? (
+            messages.map((msg, idx) => (
+              <div key={idx} className="mb-4">
+                <Message
+                  key={idx}
+                  text={msg.text}
+                  user={msg.user}
+                  added={msg.added}
+                />
+                <Link to={`/messages/${idx + 1}`} className="btn btn-info">
+                  View Message
+                </Link>
+              </div>
+            ))
+          ) : (
+            <div className="flex items-center justify-center">
+              <p>No messages to display</p>
+            </div>
+          )}
+        </>
+
+        <Dialog />
+      </main>
+      <footer className="footer footer-center bg-base-300 text-base-content p-4 mt-auto">
+        <aside>
+          <p>
+            Copyright &copy; 2023 Mini Message Board. Created By Jimmy Givans
+          </p>
+        </aside>
+      </footer>
+    </div>
+  );
+}
+
+const Dialog: React.FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [text, setText] = useState<string>("");
+  const [user, setUser] = useState<string>("");
   const dialogRef = useRef<HTMLDialogElement | null>(null);
-  const [text, setText] = useState("");
-  const [user, setUser] = useState("");
 
   useEffect(() => {
     const modal = dialogRef.current;
@@ -59,96 +104,56 @@ function Root() {
   };
 
   return (
-    <div className="flex flex-col min-h-screen">
-      <header className="my-3">
-        <h1 className="text-2xl text-center">Mini Message Board</h1>
-      </header>
-      <main className="w-2/4 my-0 mx-auto">
-        <h2 className="text-xl">Messages</h2>
+    <>
+      <button
+        type="button"
+        className="btn btn-info mb-3"
+        onClick={(e) => setIsOpen(true)}
+      >
+        Add a new message
+      </button>
 
-        <>
-          {messages.length >= 1 ? (
-            messages.map((msg, idx) => (
-              <div key={idx} className="mb-4">
-                <Message
-                  key={idx}
-                  text={msg.text}
-                  user={msg.user}
-                  added={msg.added}
+      <dialog ref={dialogRef} className="modal">
+        <div className="modal-box">
+          <form action="/api/messages" method="post" onSubmit={onFormSubmitted}>
+            <div className="my-1">
+              <label htmlFor="text" className="form-control w-full max-w-xs">
+                Text
+                <input
+                  type="text"
+                  name="text"
+                  id="text"
+                  className="input input-bordered w-full max-w-xs"
+                  value={text}
+                  onChange={(e) => setText(e.target.value)}
+                  required
                 />
-                <Link to={`/messages/${idx + 1}`} className="btn btn-info">
-                  View Message
-                </Link>
-              </div>
-            ))
-          ) : (
-            <div className="flex items-center justify-center">
-              <p>No messages to display</p>
+              </label>
             </div>
-          )}
-        </>
 
-        <button
-          type="submit"
-          className="btn btn-info mb-3"
-          onClick={(e) => setIsOpen(true)}
-        >
-          Add a new message
-        </button>
+            <div>
+              <label htmlFor="user" className="form-control w-full max-w-xs">
+                User
+                <input
+                  type="text"
+                  name="user"
+                  id="user"
+                  className="input input-bordered w-full max-w-xs"
+                  value={user}
+                  onChange={(e) => setUser(e.target.value)}
+                  required
+                />
+              </label>
+            </div>
 
-        <dialog ref={dialogRef} className="modal">
-          <div className="modal-box">
-            <form
-              action="/api/messages"
-              method="post"
-              onSubmit={onFormSubmitted}
-            >
-              <div className="my-1">
-                <label htmlFor="text" className="form-control w-full max-w-xs">
-                  Text
-                  <input
-                    type="text"
-                    name="text"
-                    id="text"
-                    className="input input-bordered w-full max-w-xs"
-                    value={text}
-                    onChange={(e) => setText(e.target.value)}
-                    required
-                  />
-                </label>
-              </div>
-
-              <div>
-                <label htmlFor="user" className="form-control w-full max-w-xs">
-                  User
-                  <input
-                    type="text"
-                    name="user"
-                    id="user"
-                    className="input input-bordered w-full max-w-xs"
-                    value={user}
-                    onChange={(e) => setUser(e.target.value)}
-                    required
-                  />
-                </label>
-              </div>
-
-              <button type="submit" className="btn btn-info mt-1.5">
-                Submit
-              </button>
-            </form>
-          </div>
-        </dialog>
-      </main>
-      <footer className="footer footer-center bg-base-300 text-base-content p-4 mt-auto">
-        <aside>
-          <p>
-            Copyright &copy; 2023 Mini Message Board. Created By Jimmy Givans
-          </p>
-        </aside>
-      </footer>
-    </div>
+            <button type="submit" className="btn btn-info mt-1.5">
+              Submit
+            </button>
+          </form>
+        </div>
+      </dialog>
+    </>
   );
-}
+};
 
 export default Root;
