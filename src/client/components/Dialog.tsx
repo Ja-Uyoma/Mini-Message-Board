9648085e909b038139cc7f2904d6cee7abd9d36b
Jimmy Givans <jauyoma.dev@gmail.com>
2024-08-20 16:30:55 +0300

    refactor: use Dialog component from Dialog.tsx
    - Update the logic in Dialog.tsx to match that of the one in
      Root.tsx
    - Delete the Dialog component implementation in Root.tsx
    - Import the Dialog component from Dialog.tsx and render it in the
      Root component

--- src/client/components/Dialog.tsx
+++ src/client/components/Dialog.tsx
@@ -1,51 +1,100 @@
-import { useEffect, useRef, useState } from "react";
-import Form from "./Form";
+import { useEffect, useRef, useState, FormEvent } from "react";
 
-function Dialog() {
+const Dialog: React.FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [text, setText] = useState<string>("");
+  const [user, setUser] = useState<string>("");
   const dialogRef = useRef<HTMLDialogElement | null>(null);
-  const [isOpen, setIsOpen] = useState(false);
-
-  const onClickHandler = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const onEscapeKeyPressed = (
-    event: React.KeyboardEvent<HTMLDialogElement>
-  ) => {
-    if (event.key === "Escape") {
-      setIsOpen(false);
-    }
-  };
 
   useEffect(() => {
-    const dialogElem = dialogRef.current;
+    const modal = dialogRef.current;
 
-    if (dialogElem && isOpen) {
-      dialogElem.showModal();
+    if (modal && isOpen) {
+      modal.showModal();
     }
 
-    if (dialogElem && !isOpen) {
-      dialogElem.close();
+    if (modal && !isOpen) {
+      modal.close();
     }
   }, [isOpen]);
 
+  const onFormSubmitted = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append("text", text);
+    formData.append("user", user);
+
+    try {
+      const res = await fetch("/api/messages", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (res.ok) {
+        const result = await res.json();
+        console.log("Success: ", result);
+        setText("");
+        setUser("");
+        setIsOpen(false);
+      }
+    } catch (err) {
+      console.error("Request failed: ", err);
+      setIsOpen(true);
+    }
+  };
+
   return (
-    <div>
+    <>
       <button
         type="button"
-        onClick={onClickHandler}
         className="btn btn-info mb-3"
+        onClick={(e) => setIsOpen(true)}
       >
         Add a new message
       </button>
 
-      <dialog ref={dialogRef} onKeyDown={onEscapeKeyPressed} className="modal">
+      <dialog ref={dialogRef} className="modal">
         <div className="modal-box">
-          <Form />
+          <form action="/api/messages" method="post" onSubmit={onFormSubmitted}>
+            <div className="my-1">
+              <label htmlFor="text" className="form-control w-full max-w-xs">
+                Text
+                <input
+                  type="text"
+                  name="text"
+                  id="text"
+                  className="input input-bordered w-full max-w-xs"
+                  value={text}
+                  onChange={(e) => setText(e.target.value)}
+                  required
+                />
+              </label>
+            </div>
+
+            <div>
+              <label htmlFor="user" className="form-control w-full max-w-xs">
+                User
+                <input
+                  type="text"
+                  name="user"
+                  id="user"
+                  className="input input-bordered w-full max-w-xs"
+                  value={user}
+                  onChange={(e) => setUser(e.target.value)}
+                  required
+                />
+              </label>
+            </div>
+
+            <button type="submit" className="btn btn-info mt-1.5">
+              Submit
+            </button>
+          </form>
         </div>
       </dialog>
-    </div>
+    </>
   );
-}
+};
 
 export default Dialog;
