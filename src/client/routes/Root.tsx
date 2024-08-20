23d0ae73647aee6c8fe3b972614918d3928dff1b
Jimmy Givans <jauyoma.dev@gmail.com>
2024-08-20 15:06:56 +0300

    feat: implement modal logic in Root component

--- src/client/routes/Root.tsx
+++ src/client/routes/Root.tsx
@@ -1,7 +1,6 @@
-import React from "react";
+import { useEffect, useRef, useState } from "react";
 
 import Message, { MessageObject } from "../components/Message";
-import Dialog from "../components/Dialog";
 import { useLoaderData } from "react-router-dom";
 
 export async function loader() {
@@ -16,6 +15,20 @@ export async function loader() {
 
 function Root() {
   const messages = useLoaderData() as MessageObject[];
+  const [isOpen, setIsOpen] = useState(false);
+  const dialogRef = useRef<HTMLDialogElement | null>(null);
+
+  useEffect(() => {
+    const modal = dialogRef.current;
+
+    if (modal && isOpen) {
+      modal.showModal();
+    }
+
+    if (modal && !isOpen) {
+      modal.close();
+    }
+  }, [isOpen]);
 
   return (
     <div className="flex flex-col min-h-screen">
@@ -42,7 +55,53 @@ function Root() {
           )}
         </>
 
-        <Dialog />
+        <button
+          type="submit"
+          className="btn btn-info mb-3"
+          onClick={(e) => setIsOpen(true)}
+        >
+          Add a new message
+        </button>
+
+        <dialog ref={dialogRef} className="modal">
+          <div className="modal-box">
+            <form
+              action="/api/messages"
+              method="post"
+              onSubmit={(e) => setIsOpen(false)}
+            >
+              <div className="my-1">
+                <label htmlFor="text" className="form-control w-full max-w-xs">
+                  Text
+                  <input
+                    type="text"
+                    name="text"
+                    id="text"
+                    className="input input-bordered w-full max-w-xs"
+                    required
+                  />
+                </label>
+              </div>
+
+              <div>
+                <label htmlFor="user" className="form-control w-full max-w-xs">
+                  User
+                  <input
+                    type="text"
+                    name="user"
+                    id="user"
+                    className="input input-bordered w-full max-w-xs"
+                    required
+                  />
+                </label>
+              </div>
+
+              <button type="submit" className="btn btn-info mt-1.5">
+                Submit
+              </button>
+            </form>
+          </div>
+        </dialog>
       </main>
       <footer className="footer footer-center bg-base-300 text-base-content p-4 mt-auto">
         <aside>
