31e9853b12fb6919c8fc7a660a7eb32eb56a4fef
Jimmy Givans <jauyoma.dev@gmail.com>
2024-08-20 17:04:13 +0300

    refactor: delete unused components' source files

--- src/client/components/Loading.tsx
+++
@@ -1,9 +0,0 @@
-function Loading() {
-  return (
-    <div className="flex justify-center">
-      <p className="text-3xl">Loading...</p>
-    </div>
-  );
-}
-
-export default Loading;
--- src/client/components/NetworkError.tsx
+++
@@ -1,9 +0,0 @@
-function NetworkError() {
-  return (
-    <div className="flex justify-center">
-      <p className="text-2xl">A network error was encountered</p>
-    </div>
-  );
-}
-
-export default NetworkError;
